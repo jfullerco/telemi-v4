93bc8b969aaaa83dfec22d4b8a6796128bf0d116
jfullerco <jonathan@jfuller.co>
2021-08-23 00:18:24 -0400

    1 file modified

--- src/Components/Graphs/CostBySite.js
+++ src/Components/Graphs/CostBySite.js
@@ -13,12 +13,12 @@ const CostBySite = () => {
   }, {}) : ""
 
   const groupLocations = grouper(services)
-
+ 
 
   return(
     <>
     <ResponsivePie
-      data={groupLocations && groupLocations}
+      data={groupLocations}
       margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
       innerRadius={0.5}
       padAngle={0.7}
