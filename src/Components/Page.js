aeda4c5491149121a2d9c6e24847bd42a137f865
jfullerco <jonathan@jfuller.co>
2021-08-26 02:45:24 -0400

    1 file modified

--- src/Components/Page.js
+++ src/Components/Page.js
@@ -16,13 +16,13 @@ const Page = ({title, subtitle, handleGoBack, pageError, pageSuccess, children})
       <div className="content pt-5 pb-5" >
         <Columns options="is-mobile is-vcentered">
           <Column size="is-narrow">
-            <FaArrowLeft className="icon pl-1 mx-2 my-2" onClick={() => handleGoBack()}/> 
+            <div className="icon is-large"><FaArrowLeft className="icon pl-1 mx-2 my-2" onClick={() => handleGoBack()}/> </div>
           </Column>
           <Column>
-            <span className="title pl-2">{title}</span>
+            <span className="title"><div className="donotwrap">{title}</div></span>
           </Column>
         </Columns>
-      </div>
+      </div> 
           
       <div>
         <div className={pageSuccess && pageSuccess != false ? success : "is-hidden"}>
