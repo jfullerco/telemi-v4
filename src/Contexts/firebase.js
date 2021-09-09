b610557deba96327cf4a1a6ecbcc982b8c922bf1
jfullerco <jonathan@jfuller.co>
2021-09-09 13:09:12 -0400

    1 file modified

--- src/Contexts/firebase.js
+++ src/Contexts/firebase.js
@@ -1,28 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { auth } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import * as firebase from 'firebase/app'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCLcH3HfDZU-0Qa8k90hHYhQZZRyOAy8j4",
-  authDomain: "tiems-24d17.firebaseapp.com",
-  databaseURL: "https://tiems-24d17-default-rtdb.firebaseio.com",
-  projectId: "tiems-24d17",
-  storageBucket: "tiems-24d17.appspot.com",
-  messagingSenderId: "371736927443",
-  appId: "1:371736927443:web:9d80c3abc38e399db3c826",
-  measurementId: "G-1R2QK5MPPK"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyBi2EZr6TAuP_M0GDf2t6kYjVSBr-EEHgI",
+  authDomain: "telemi-develop.firebaseapp.com",
+  projectId: "telemi-develop",
+  storageBucket: "telemi-develop.appspot.com",
+  messagingSenderId: "208473651772",
+  appId: "1:208473651772:web:b7d880afd16985ad626139",
+  measurementId: "G-K3F50P2C8N"
+})
 
-export const auth = app.auth()
+export const auth = app.auth() 
 export const db = app.firestore()
 export const store = app.storage()
 
