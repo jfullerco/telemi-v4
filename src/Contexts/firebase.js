a8b4ff86c45ca8107076bcd5dd789fe5c1088b4f
jfullerco <jonathan@jfuller.co>
2021-09-26 20:30:02 -0400

    1 file modified

--- src/Contexts/firebase.js
+++ src/Contexts/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from 'firebase/auth'
+import { getAuth } from 'firebase/auth';
 import { 
   getFirestore, 
   collection, 
@@ -50,14 +50,14 @@ export const fire = {
   uploadBytes,
   arrayUnion,
   arrayRemove
-} 
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const auth = getAuth(app) 
-export const db = getFirestore(app)
-export const store = getStorage(app) 
+export const auth = getAuth(app); 
+export const db = getFirestore(app);
+export const store = getStorage(app); 
 
 
 export default app
